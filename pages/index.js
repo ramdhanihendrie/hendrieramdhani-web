cb9855eb80cf313abac5d9598b2b899e7e45f8fa
ramdhanihendrie <50083363+ramdhanihendrie@users.noreply.github.com>
2022-04-01 09:44:56 +0700

    Custom document for Font Optimization

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Hendrie Ramdhani</title>
         <meta name="description" content="Hendrie Ramdhani" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      Home
     </div>
   )
 }
