93be95a56cdb3d385d18035246e237d97d417c3f
ramdhanihendrie <50083363+ramdhanihendrie@users.noreply.github.com>
2022-04-05 22:43:29 +0700

    Create navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,8 +1,77 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const navbar = () => {
+  const [menuOpen, setMenuOpen] = React.useState(false);
   return (
-    <div>navbar</div>
+    <>
+      <div className="flex flex-wrap">
+        <div className="w-full">
+          <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-darken">
+            <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+              <div className="w-full relative flex justify-between lg:w-auto px-4 lg:static lg:block lg:justify-start">
+                <a
+                  className="text-xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap text-primary"
+                  href="#"
+                >
+                  Hendrie Ramdhani
+                </a>
+                <button
+                  className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                  type="button"
+                  onClick={() => setMenuOpen(!menuOpen)}
+                >
+                  <FontAwesomeIcon icon={faBars} />
+                </button>
+              </div>
+              <div
+                className={
+                  "lg:flex flex-grow items-center" +
+                  (menuOpen ? " flex" : " hidden")
+                }
+                id="example-navbar-info"
+              >
+                <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+                  <li className="nav-item">
+                    <a
+                      className="px-3 py-2 flex items-center text-lg font-semibold leading-snug text-white hover:opacity-75"
+                      href="#"
+                    >
+                    Home
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="px-3 py-2 flex items-center text-lg font-semibold leading-snug text-white hover:opacity-75"
+                      href="#about"
+                    >
+                      About Me
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="px-3 py-2 flex items-center text-lg font-semibold leading-snug text-white hover:opacity-75"
+                      href="#projects"
+                    >
+                      Projects
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="px-3 py-2 flex items-center text-lg font-semibold leading-snug text-white hover:opacity-75"
+                      href="#contact"
+                    >
+                      Contact
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </>
   )
 }
 
