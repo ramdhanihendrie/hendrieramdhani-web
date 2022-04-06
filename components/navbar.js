1054b5e595915c8ce6783c29dfb55fa270584c9c
ramdhanihendrie <50083363+ramdhanihendrie@users.noreply.github.com>
2022-04-07 05:20:10 +0700

    navbar padding/container fix

--- components/navbar.js
+++ components/navbar.js
@@ -9,11 +9,11 @@ const navbar = () => {
     <>
       <div className="flex flex-wrap">
         <div className="w-full">
-          <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-darken">
-            <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-              <div className="w-full relative flex justify-between lg:w-auto px-4 lg:static lg:block lg:justify-start">
+          <nav className="relative flex flex-wrap items-center justify-between py-2 pl-3 md:pl-0  bg-darken">
+            <div className="container mx-auto flex flex-wrap items-center justify-between">
+              <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
                 <a
-                  className="text-3xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap text-primary"
+                  className="text-xl md:text-3xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap text-primary"
                   href="#"
                 >
                   Hendrie Ramdhani
