cef32700718c5a8d0d4fef936b36114d0e154e60
ramdhanihendrie <50083363+ramdhanihendrie@users.noreply.github.com>
2022-04-08 00:26:42 +0700

    Add next/link to navbar menu

--- components/navbar.js
+++ components/navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 
@@ -12,12 +13,13 @@ const navbar = () => {
           <nav className="relative flex flex-wrap items-center justify-between py-2 pl-3 md:pl-0  bg-darken">
             <div className="container mx-auto flex flex-wrap items-center justify-between">
               <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-                <a
-                  className="text-xl md:text-3xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap text-primary"
-                  href="#"
-                >
-                  Hendrie Ramdhani
-                </a>
+                <Link href="/">
+                  <a
+                    className="text-xl md:text-3xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap text-primary"
+                  >
+                    Hendrie Ramdhani
+                  </a>
+                </Link>
                 <button
                   className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
                   type="button"
@@ -38,12 +40,13 @@ const navbar = () => {
                     navs.map((nav) => {
                       return (
                         <li className="nav-item" key={nav}>
-                          <a
-                            className="px-3 py-2 flex items-center text-lg font-semibold leading-snug text-white hover:opacity-75 capitalize"
-                            href={"#" + (nav == "home" ? "" : nav)}
-                          >
-                            {nav}
-                          </a>
+                          <Link href={"#" + (nav == "home" ? "" : nav)}>
+                            <a
+                              className="px-3 py-2 flex items-center text-lg font-semibold leading-snug text-white hover:opacity-75 capitalize"
+                            >
+                              {nav}
+                            </a>
+                          </Link>
                         </li>
                       )
                     })
