5314825c0b7791a29d75e7e55dd31a9e768802f0
ramdhanihendrie <50083363+ramdhanihendrie@users.noreply.github.com>
2022-04-11 11:04:12 +0700

    Add smooth scroll

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html style={{scrollBehavior:'smooth'}}>
         <Head>
           <link 
             href="https://fonts.googleapis.com/css2?family=Poppins" 
