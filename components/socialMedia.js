443f38ca2dc55bbb0d234cc05469bda6e5832821
ramdhanihendrie <50083363+ramdhanihendrie@users.noreply.github.com>
2022-04-15 04:32:51 +0700

    add rel to anchor in socialmedia components

--- components/socialMedia.js
+++ components/socialMedia.js
@@ -4,16 +4,16 @@ import { Icon } from '@iconify/react';
 const socialMedia = () => {
   return (
     <div className="grid grid-flow-col text-primary gap-3 mt-2 justify-center sm:justify-start">
-      <a href="https://www.github.com/ramdhanihendrie/" target="_blank">
+      <a href="https://www.github.com/ramdhanihendrie/" target="_blank" rel="noreferrer">
         <Icon icon="akar-icons:github-fill" width={36}/>
       </a>
-      <a href="https://www.linkedin.com/in/hendrieramdhani/" target="_blank">
+      <a href="https://www.linkedin.com/in/hendrieramdhani/" target="_blank" rel="noreferrer">
         <Icon icon="akar-icons:linkedin-box-fill" width={36}/>
       </a>
-      <a href="https://www.instagram.com/ramdhanihendrie/" target="_blank">
+      <a href="https://www.instagram.com/ramdhanihendrie/" target="_blank" rel="noreferrer">
         <Icon icon="akar-icons:instagram-fill" width={36}/>
       </a>
-      <a href="https://www.twitter.com/ramdhanihendrie/" target="_blank">
+      <a href="https://www.twitter.com/ramdhanihendrie/" target="_blank" rel="noreferrer">
         <Icon icon="akar-icons:twitter-fill" width={36}/>
       </a>
     </div>
