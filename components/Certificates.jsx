3c9f9898744d1c8b4c006edb874a1be9d9f56324
ramdhanihendrie <50083363+ramdhanihendrie@users.noreply.github.com>
2023-01-28 09:42:46 +0700

    fix certificate

--- components/Certificates.jsx
+++ components/Certificates.jsx
@@ -5,14 +5,14 @@ const Certificates = () => {
     {id: "1", name: "React JS for Web Development"},
     {id: "2", name: "Front End Developer"},
     {id: "3", name: "The Complete Front End Web Development"},
-    {id: "4", name: "Belajar Dasar Pemrograman Web"},
+    {id: "4", name: "Speaker WebDev Class"},
     {id: "5", name: "Web Development Course NodeJS"},
-    {id: "6", name: "React"},
+    {id: "6", name: "Belajar Dasar Pemrograman Web"},
   ]
   return (
     <section>
       <h3 className="text-primary font-semibold text-2xl mt-20">Certificates:</h3>
-      <p className="text-justify tracking-wide leading-7 mt-3">I have learned several skills through courses and certifications, here are some of the skill certificates I have.</p>
+      <p className="text-justify tracking-wide leading-7 mt-3">I have learned several skills through courses, certifications and other events, here are some of the skill certificates I have.</p>
       <div className="columns-1 md:columns-2 gap-0 xl:px-40">
         {
           certs.map((certs) => {
